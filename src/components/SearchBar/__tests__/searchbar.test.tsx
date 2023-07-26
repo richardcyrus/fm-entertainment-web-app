3ea0d9c1503235cd63c82f87958ed24918d83eed
Richard Cyrus <richard.cyrus@rcyrus.com>
2023-07-26 16:40:14 -0400

    refactor: test that router has been called

--- src/components/SearchBar/__tests__/searchbar.test.tsx
+++ src/components/SearchBar/__tests__/searchbar.test.tsx
@@ -1,19 +1,23 @@
 import { describe, expect, it } from '@jest/globals'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { axe } from 'jest-axe'
-// import mockRouter from 'next-router-mock/async'
 
 import { SearchBar } from '@/components/SearchBar'
 import { ShowCategory } from '@/types'
 
+const useRouter = jest.spyOn(require('next/navigation'), 'useRouter')
+
 describe('Search Bar', () => {
   let expectedLabel: string
   let expectedCategory: ShowCategory
+  let expectedRouterPush: jest.Mock
 
   beforeEach(() => {
     expectedLabel = 'Search for movies or TV series'
     expectedCategory = 'All'
+    expectedRouterPush = jest.fn()
+    useRouter.mockReturnValue({ push: expectedRouterPush })
   })
 
   it('renders a search landmark', async () => {
@@ -36,13 +40,9 @@ describe('Search Bar', () => {
     const searchBox = screen.getByRole('searchbox')
     await user.type(searchBox, 'Beyond Earth')
 
+    await waitFor(() => {
+      expect(expectedRouterPush).toHaveBeenCalledTimes(1)
+    })
     expect(searchBox).toHaveValue('Beyond Earth')
-    // await waitFor(() => {
-    //   expect(mockRouter).toMatchObject({
-    //     pathname: '/',
-    //     asPath: '/?search=All&term=Beyond+Earth',
-    //     query: { search: 'All', term: 'Beyond Earth' },
-    //   })
-    // })
   })
 })
