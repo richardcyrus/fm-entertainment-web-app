f3b9fdc8d9d91c47e6f57e7291367cf55a630f58
Richard Cyrus <richard.cyrus@rcyrus.com>
2023-09-28 20:06:02 -0400

    feat: add functions to get the various media types

--- src/models/videos.ts
+++ src/models/videos.ts
@@ -9,6 +9,57 @@ export async function getTrendingShows() {
 
   return prisma.video.findMany({
     where: whereCriteria,
-    orderBy: [{ id: 'desc' }],
+  })
+}
+
+export async function getRecommendedShows() {
+  const whereCriteria = Prisma.validator<Prisma.VideoWhereInput>()({
+    isTrending: false,
+  })
+
+  return prisma.video.findMany({
+    where: whereCriteria,
+  })
+}
+
+export async function getMovies() {
+  const whereCriteria = Prisma.validator<Prisma.VideoWhereInput>()({
+    category: 'Movie',
+  })
+
+  return prisma.video.findMany({
+    where: whereCriteria,
+  })
+}
+
+export async function getTVSeries() {
+  const whereCriteria = Prisma.validator<Prisma.VideoWhereInput>()({
+    category: 'TV Series',
+  })
+
+  return prisma.video.findMany({
+    where: whereCriteria,
+  })
+}
+
+export async function getBookmarkedMovies() {
+  const whereCriteria = Prisma.validator<Prisma.VideoWhereInput>()({
+    isBookmarked: true,
+    category: 'Movie',
+  })
+
+  return prisma.video.findMany({
+    where: whereCriteria,
+  })
+}
+
+export async function getBookmarkedTVSeries() {
+  const whereCriteria = Prisma.validator<Prisma.VideoWhereInput>()({
+    isBookmarked: true,
+    category: 'TV Series',
+  })
+
+  return prisma.video.findMany({
+    where: whereCriteria,
   })
 }
