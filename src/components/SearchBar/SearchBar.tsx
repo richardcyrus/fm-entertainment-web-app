0ca6d052e7e4a7f36efa33e6005dd905216b8b00
Richard Cyrus <richard.cyrus@rcyrus.com>
2023-10-12 22:38:58 -0400

    refactor: change form method to GET
    - use a form reference to trigger submit after input debounce

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import { ChangeEvent, useState } from 'react'
-
-import { usePathname, useRouter } from 'next/navigation'
+import { ChangeEvent, useState, useRef } from 'react'
 
 import SearchIcon from '@/assets/icon-search.svg'
 import useDebounce from '@/hooks/useDebounce'
@@ -13,33 +11,35 @@ import styles from './searchbar.module.css'
 export function SearchBar({ label, category }: SearchBarProps) {
   const [searchTerm, setSearchTerm] = useState<string>('')
 
-  const pathname = usePathname()
-  const router = useRouter()
-  const searchParams = new URLSearchParams()
-  searchParams.append('search', category)
+  const formRef = useRef<HTMLFormElement>(null)
 
   const onInputChanged = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value)
   }
 
-  const debouncedSearchTerm = useDebounce(searchTerm, 500)
+  const debouncedSearchTerm = useDebounce(searchTerm)
 
-  if (debouncedSearchTerm !== '') {
-    searchParams.append('term', searchTerm)
-    router.push(`${pathname}?${searchParams.toString()}`)
+  if (debouncedSearchTerm) {
+    formRef.current?.submit()
   }
 
   return (
     <div className="search-container">
-      <form role="search" className={styles.searchForm}>
+      <form
+        role="search"
+        className={styles['search-form']}
+        ref={formRef}
+        method="GET"
+      >
+        <input type="hidden" name="category" value={category} />
         <label htmlFor="search" aria-label={label}>
-          <SearchIcon className={styles.searchIcon} />
+          <SearchIcon className={styles['search-icon']} />
         </label>
         <input
-          className={styles.searchInput}
+          className={styles['search-input']}
           type="search"
           id="search"
-          name="search"
+          name="title"
           placeholder={label}
           onChange={onInputChanged}
         />
--- src/components/SearchBar/__tests__/searchbar.test.tsx
+++ src/components/SearchBar/__tests__/searchbar.test.tsx
@@ -1,23 +1,17 @@
 import { describe, expect, it } from '@jest/globals'
-import { render, screen, waitFor } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
 import { axe } from 'jest-axe'
 
 import { SearchBar } from '@/components/SearchBar'
-import { ShowCategory } from '@/types'
-
-const useRouter = jest.spyOn(require('next/navigation'), 'useRouter')
+import type { ShowCategory } from '@/types'
 
 describe('Search Bar', () => {
   let expectedLabel: string
   let expectedCategory: ShowCategory
-  let expectedRouterPush: jest.Mock
 
   beforeEach(() => {
     expectedLabel = 'Search for movies or TV series'
     expectedCategory = 'All'
-    expectedRouterPush = jest.fn()
-    useRouter.mockReturnValue({ push: expectedRouterPush })
   })
 
   it('renders a search landmark', async () => {
@@ -29,23 +23,5 @@ describe('Search Bar', () => {
 
     expect(searchBar).toBeInTheDocument()
     expect(await axe(container)).toHaveNoViolations()
-  })
-
-  it('can enter a search term', async () => {
-    const { container } = render(
-      <SearchBar label={expectedLabel} category={expectedCategory} />
-    )
-    const user = userEvent.setup()
-
-    const searchBox = screen.getByRole('searchbox')
-    await user.type(searchBox, 'Beyond Earth')
-
-    await waitFor(() => {
-      expect(expectedRouterPush).toHaveBeenCalledTimes(1)
-    })
-    expect(expectedRouterPush).toHaveBeenCalledWith(
-      '/?search=All&term=Beyond+Earth'
-    )
-    expect(searchBox).toHaveValue('Beyond Earth')
   })
 })
